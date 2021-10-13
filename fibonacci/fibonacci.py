3b45216871fbec5ffd5c26c2b7a71f6f4dff81aa
mvsrgc <14826113+mvsrgc@users.noreply.github.com>
2021-10-13 08:24:01 -0400

    Fibonacci with generator.

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,14 +1,18 @@
-def fibonacci(n: int) -> int:
-    if n == 0: return n
+from typing import Generator
+
+
+def fibonacci(n: int) -> Generator[int, None, None]:
+    yield 0
+    if n > 0:
+        yield 1
+
     last: int = 0
     next: int = 1
 
     for _ in range(1, n):
         last, next = next, last + next
-
-    return next
+        yield next
 
 
 if __name__ == "__main__":
-    for i in range(50):
-        print(fibonacci(i))
+    print(list(fibonacci(50)))
